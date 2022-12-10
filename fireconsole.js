fefb9a1a427705da602a4930c6700780e71d3f3b
bbarni2020 <75606424+bbarni2020@users.noreply.github.com>
2022-12-10 17:28:11 +0100

    Add files via upload

--- fireconsole.js
+++ fireconsole.js
@@ -1,11 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyDtK9A1EZGdG28qPbFrbU4E7lnhi5IK5Vc",
   authDomain: "smartwidgets-website.firebaseapp.com",
@@ -16,6 +13,28 @@ const firebaseConfig = {
   measurementId: "G-TXEHCJ8JF0"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+
+var emailInput = document.getElementById('username');
+var passwordInput = document.getElementById('password');
+var loginButton = document.getElementById('login');
+
+
+loginButton.addEventListener('click', function() {
+
+  var email = emailInput.value;
+  var password = passwordInput.value;
+
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(function() {
+      console.log("Hello")
+    })
+    .catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorCode + ': ' + errorMessage);
+    });
+});
